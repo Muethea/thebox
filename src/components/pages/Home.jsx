010d64110ce593d3d16f96ddcb8709ac589b452c
Fernando dos Santos Muethea <64161845+Muethea@users.noreply.github.com>
2022-10-03 13:35:56 +0200

    up

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,7 +2,18 @@ import React from "react";
 import service from "../layout/imgs/Vector.png";
 import design from "../layout/imgs/Vector2.png";
 import imgAbout from "../layout/imgs/about.png";
-import { BiPaintRoll } from "react-icons/bi";
+import project1 from "../layout/imgs/project1.png";
+import project2 from "../layout/imgs/project2.png";
+import project3 from "../layout/imgs/project3.png";
+import project4 from "../layout/imgs/project4.png";
+import { BiBuildings } from "react-icons/bi";
+import { BiBriefcase } from "react-icons/bi";
+import { MdElectricalServices } from "react-icons/md";
+import { MdArchitecture } from "react-icons/md";
+import { FaHandshake } from "react-icons/fa";
+import { BsArrowLeftRight } from "react-icons/bs";
+import { AiOutlineColumnHeight } from "react-icons/ai";
+
 import "./styles/style.css";
 
 const Home = () => {
@@ -62,32 +73,161 @@ const Home = () => {
 
           <div className="cards1 container">
             <div className="card font">
-              <BiPaintRoll className="icons" />
+              <i>
+                {" "}
+                <BiBuildings />
+              </i>
               <hr className="linha" />
               <h2>Construction</h2>
             </div>
             <div className="card back">
+              <i>
+                {" "}
+                <BsArrowLeftRight />
+              </i>
+
               <hr className="linha" />
               <h2>Renovation</h2>
             </div>
             <div className="card font">
+              <i>
+                {" "}
+                <FaHandshake />
+              </i>
+
               <hr className="linha" />
               <h2>Consultation</h2>
             </div>
           </div>
           <div className="cards2 container">
             <div className="card  back">
+              <i>
+                {" "}
+                <BiBriefcase />
+              </i>
+
               <hr className="linha" />
               <h2>Repair Services</h2>
             </div>
             <div className="card font">
+              <i>
+                {" "}
+                <MdArchitecture />
+              </i>
               <hr className="linha" />
               <h2>Architecture</h2>
             </div>
             <div className="card back">
+              <i>
+                {" "}
+                <MdElectricalServices />
+              </i>
               <hr className="linha" />
               <h2>Electric</h2>
             </div>
+          </div>
+        </section>
+
+        <section className="experience ">
+          <div className="cardsexperience">
+            <div className="experience1 container">
+              <div className="card ">
+                <h2>123</h2>
+                <p className="arrow">
+                  <i>
+                    <AiOutlineColumnHeight />
+                  </i>
+                  Projects Completed
+                </p>
+              </div>
+              <div className="card card2">
+                <h2>84</h2>
+                <p className="arrow">
+                  <i>
+                    <AiOutlineColumnHeight />
+                  </i>
+                  Happy Clients
+                </p>
+              </div>
+            </div>
+            <div className="experience2 container">
+              <div className="card  ">
+                <h2>30</h2>
+                <p className="arrow">
+                  <i>
+                    {" "}
+                    <AiOutlineColumnHeight />
+                  </i>
+                  Years in Business
+                </p>
+              </div>
+              <div className="card ">
+                <h2>37</h2>
+                <p className="arrow">
+                  <i>
+                    {" "}
+                    <AiOutlineColumnHeight />
+                  </i>{" "}
+                  Awards Win
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="infoexperience">
+            <h1>30 Years Experience</h1>
+            <p>
+              Our company has been the leading provided construction services to
+              clients throughout the USA since 1988.
+            </p>
+            <button>Contact Us</button>
+          </div>
+        </section>
+
+        <section className="section3">
+          <div className="info container">
+            <div>
+              <h2>Free consultation with exceptional quality</h2>
+            </div>
+            <div className="btn">
+              <button>Get your consultation</button>
+            </div>
+          </div>
+        </section>
+        <section className="section4">
+          <div className="projects container">
+            <div className="inf">
+              <h2>Projects</h2>
+
+              <h3>All</h3>
+            </div>
+
+            <div className="projects">
+              <div className="project12">
+                <div className="projects1">
+                  <img src={project1} alt="" />
+                </div>
+                <div className="projects2">
+                  <img src={project2} alt="" />
+                </div>
+              </div>
+              <div className="project">
+                <div className="projects3">
+                  <img src={project3} alt="" />
+                </div>
+                <div className="projects4">
+                  <img src={project4} alt="" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="section5">
+          <div className="info container">
+            <div>
+              <h2>Free consultation with exceptional quality</h2>
+            </div>
+            <div className="btn"></div>
           </div>
         </section>
       </main>
--- src/components/pages/Services.jsx
+++ src/components/pages/Services.jsx
@@ -1,5 +1,10 @@
 import React from "react";
-
+import { BiBuildings } from "react-icons/bi";
+import { BiBriefcase } from "react-icons/bi";
+import { MdElectricalServices } from "react-icons/md";
+import { MdArchitecture } from "react-icons/md";
+import { FaHandshake } from "react-icons/fa";
+import { BsArrowLeftRight } from "react-icons/bs";
 const Services = () => {
   return (
     <div>
@@ -8,28 +13,55 @@ const Services = () => {
 
         <div className="cards1 container">
           <div className="card font">
+            <i>
+              {" "}
+              <BiBuildings />
+            </i>
             <hr className="linha" />
             <h2>Construction</h2>
           </div>
           <div className="card back">
+            <i>
+              {" "}
+              <BsArrowLeftRight />
+            </i>
+
             <hr className="linha" />
             <h2>Renovation</h2>
           </div>
           <div className="card font">
+            <i>
+              {" "}
+              <FaHandshake />
+            </i>
+
             <hr className="linha" />
             <h2>Consultation</h2>
           </div>
         </div>
         <div className="cards2 container">
           <div className="card  back">
+            <i>
+              {" "}
+              <BiBriefcase />
+            </i>
+
             <hr className="linha" />
             <h2>Repair Services</h2>
           </div>
           <div className="card font">
+            <i>
+              {" "}
+              <MdArchitecture />
+            </i>
             <hr className="linha" />
             <h2>Architecture</h2>
           </div>
           <div className="card back">
+            <i>
+              {" "}
+              <MdElectricalServices />
+            </i>
             <hr className="linha" />
             <h2>Electric</h2>
           </div>
